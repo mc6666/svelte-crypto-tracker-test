b69b9238f82ce26c31769465646590d8a55d514c
Happy Coder <mc6666@gmail.com>
2021-06-01 16:13:50 +0800

    v1

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 app.listen(port, () =>
-  console.log(Server is running on: http://localhost:${port}`));
+  console.log('Server is running on: http://localhost:${port}'));
   
